b193a3d9e39695baf1c7fd453c0deac85cad0170
fuuga000 <fuga0827fu@gmail.com>
2024-04-09 12:27:19 +0900

    最新版をインストールするようにupdate

--- script/install.sh
+++ script/install.sh
@@ -5,14 +5,13 @@
 set -e
 
 OS=$(uname -s)
-VERSION="0.1.1"
 ARCH=$(uname -m)
 
 INSTALL_PATH=/usr/local/bin/mgpm
 if [ ${ARCH} = "arm64" ]; then
   ARCH="aarch64"
 fi
-URL=https://github.com/mountain-gorilla-dev/mgpm/releases/download/$VERSION/mgpm-$OS-$ARCH
+URL=https://github.com/mountain-gorilla-dev/mgpm/releases/latest/download/mgpm-$OS-$ARCH
 wget -O $INSTALL_PATH $URL
 chmod +x $INSTALL_PATH
 exit
