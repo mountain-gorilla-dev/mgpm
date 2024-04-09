90a639af1182675496e4c92257df64b6b4f9265e
fuuga000 <fuga0827fu@gmail.com>
2024-04-09 11:49:47 +0900

    linux対応

--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# linux
+# mac&linux
 
 set -e
 
@@ -8,20 +8,11 @@ OS=$(uname -s)
 VERSION="0.1.1"
 ARCH=$(uname -m)
 
-if [ ${OS} = "Darwin" ]; then
-  echo "Mac OS です"
-  INSTALL_PATH=/usr/local/bin/mgpm
-  if [ ${ARCH} = "arm64" ]; then
-    ARCH="aarch64"
-  fi
-  URL=https://github.com/mountain-gorilla-dev/mgpm/releases/download/$VERSION/mgpm-darwin-$ARCH
-  wget -O $INSTALL_PATH $URL
-  chmod +x $INSTALL_PATH
-  exit
-elif [ ${OS} = "Linux" ]; then
-  echo "Linuxです"
-  exit
-else
-  echo "対応していないOSです"
-  exit
+INSTALL_PATH=/usr/local/bin/mgpm
+if [ ${ARCH} = "arm64" ]; then
+  ARCH="aarch64"
 fi
+URL=https://github.com/mountain-gorilla-dev/mgpm/releases/download/$VERSION/mgpm-$OS-$ARCH
+wget -O $INSTALL_PATH $URL
+chmod +x $INSTALL_PATH
+exit
