48487aa3d5833296e8b203b4f27d26ae4a392b23
fuuga000 <fuga0827fu@gmail.com>
2024-05-07 11:44:14 +0900

    update

--- script/install.sh
+++ script/install.sh
@@ -8,6 +8,8 @@ OS=$(uname -s)
 ARCH=$(uname -m)
 
 INSTALL_PATH=/usr/local/bin/mgpm
+LIB_PATH=/var/lib/mgpm
+git clone https://github.com/mountain-gorilla-dev/mgpm.git $LIB_PATH
 if [ ${ARCH} = "arm64" ]; then
   ARCH="aarch64"
 fi
